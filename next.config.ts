e13262f17fcb561c589179e68511559ba94b1fa8
root <phamminhhung20024003@gmail.com>
2025-06-30 20:34:58 +0700

    disable noti error eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+
   /* config options here */
+ eslint: {
+    // Bỏ qua mọi lỗi ESLint khi build
+    ignoreDuringBuilds: true,
+  },
+  /* các config khác */
+
 };
 
 export default nextConfig;
